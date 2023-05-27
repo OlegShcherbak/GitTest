c6f74af84aad52298ba325540cea1b517fdff6fc
Oleg Shcherbak <boec603@gmail.com>
2023-05-27 13:04:41 +0300

    Add new button

---
+++ main.js
@@ -0,0 +1,18 @@
+.my-button {
+  width: 240px;
+  height: 60px;
+  background: #9be481;
+  border: 2px solid;
+  border-color: #9be481;
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+  border-bottom-left-radius: 90% 100%;
+  border-bottom-right-radius: 90% 100%;
+  border-top-left-radius: 90% 100%;
+  border-top-right-radius: 90% 100%;
+  transition: transform 0.3s ease;
+}
+
+.my-button:hover {
+  transform: rotate(4deg);
